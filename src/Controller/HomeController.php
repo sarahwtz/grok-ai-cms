a1560321adac00fb727d26d0b02139f25d1c3f93
Sarah Wentzel <sarahwtz@gmail.com>
2025-11-20 20:53:48 +0100

    feat(home): add HomeController with index route

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(ContentRepository $repository): Response
+    {
+        $posts = $repository->findBy(['approved' => true]);
+        return $this->render('home/index.html.twig', [
+            "post" => $posts,
+        ]);
+    }
+}
